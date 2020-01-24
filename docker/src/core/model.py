37f33c6c82ecf9d955dceb0405e9e6149fefa2cf
ptuls <anamolous.behaviour@gmail.com>
2020-01-25 10:08:25 +1100

    Added model script

---
+++ docker/src/core/model.py
@@ -0,0 +1,13 @@
+import torch
+from transformers import GPT2Model, GPT2Tokenizer
+
+PRETRAINED_WEIGHTS = "gpt2"
+
+
+def main():
+    tokenizer = GPT2Tokenizer.from_pretrained(PRETRAINED_WEIGHTS)
+    model = GPT2Model.from_pretrained(PRETRAINED_WEIGHTS)
+
+
+if __name__ == "__main__":
+    main()
